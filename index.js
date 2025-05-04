f06e690422b3429356c6b698dc0691b487d1d2ce
unknown <manijangra2000@gmail.com>
2025-05-04 13:35:05 +0530

    change cors

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-    },{
+app.use(cors(
+    // {
+    // origin: 'http://localhost:5173',
+    // credentials: true
+    // },
+    {
         origin: 'https://jira-dashboard-frontend-kappa.vercel.app/',
         credentials: true
     }));
