4b25e28c7b8cf58c9f1d88d7b25f3dd820475008
unknown <manijangra2000@gmail.com>
2025-05-04 13:30:00 +0530

    add cors for vercel

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true
+    },{
+        origin: 'https://jira-dashboard-frontend-kappa.vercel.app/',
+        credentials: true
     }));
 app.use(express.json());
 
